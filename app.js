1059ab0ada867f0a47637880dc434981f2ca6b90
Steffen Nissen <Steffen.fann@gmail.com>
2019-12-12 19:40:21 +0000

    yeah

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ var text_tone = {
 var curr_docs = {};
 var Cloudant = require('@cloudant/cloudant');
 
-/*cloudant = Cloudant("https://0ec7cd4c-48d4-42e1-9f09-f63e1622f78e-bluemix:630e8f71457423bf586a43c1fc65165708db339e76a1937a955508a1a3459fa4@0ec7cd4c-48d4-42e1-9f09-f63e1622f78e-bluemix.cloudantnosqldb.appdomain.cloud");
+cloudant = Cloudant("https://438e9fc6-506b-4b2f-be54-9673dd889805-bluemix:52b882b77793b6b89338e612172bb2ccb197ffcf49ac4b7363ec9c09d0beee6f@438e9fc6-506b-4b2f-be54-9673dd889805-bluemix.cloudantnosqldb.appdomain.cloud");
 mydb = cloudant.db.use('test');
  
 mydb.list({ include_docs: true }, function(err, body) {
@@ -47,7 +47,7 @@ mydb.list({ include_docs: true }, function(err, body) {
     	curr_docs = body.rows;
     	console.log(JSON.stringify(curr_docs[0].doc));
     }
-});*/
+});
 var test_res_tone = {};
 app.get('/myform', function(req, res){ 
     myText = req.query.mytext; //mytext is the name of your input box
@@ -58,7 +58,7 @@ app.get('/myform', function(req, res){
     	content_type: 'application/json',
     };
 
-	/*toneAnalyzer.tone(toneParams, function(req, res2){
+	toneAnalyzer.tone(toneParams, function(req, res2){
         test_res_tone = res2;
         console.log(test_res_tone);
         try{
@@ -81,7 +81,7 @@ app.get('/myform', function(req, res){
 		res.render('tones',{text_tone:text_tone});
 		mydb.insert(text_tone);
 		console.log(text_tone);
-	});*/
+	});
 }); 
 
 app.get('/', function(req, res) {
